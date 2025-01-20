1cf887858a8f2f8fe2bb56cd5e88d135e8ecdf3a
Le Hoan Hao <hao.le@playnext-lab.co.jp>
2025-01-20 23:09:49 +0900

    update

--- app/stores/auth.ts
+++ app/stores/auth.ts
@@ -1,60 +1,60 @@
-import { jwtDecode } from "jwt-decode";
+import { jwtDecode } from 'jwt-decode'
 
-export const useAuthStore = defineStore("authStore", {
+export const useAuthStore = defineStore('authStore', {
   persist: [
     {
-      paths: ["token"],
-      storage: window?.sessionStorage,
-    },
+      paths: ['token'],
+      storage: window?.sessionStorage
+    }
   ],
   state: () => ({
-    token: "",
+    token: '',
     user: null,
     loadings: {} as Record<string, any>,
-    errors: {} as Record<string, any>,
+    errors: {} as Record<string, any>
   }),
   getters: {
-    authenticated: (state) => !!state.token,
+    authenticated: state => !!state.token
   },
   actions: {
     async login(tenant_id: string, username: string, password: string) {
       try {
-        this.loadings.login = true;
-        this.errors.login = null;
+        this.loadings.login = true
+        this.errors.login = null
         const response = await useAPI().adminService.post(
-          "/login",
+          '/login',
           {
             tenant_id,
             username,
-            password,
+            password
           },
           {}
-        );
-        this.token = response.data?.token;
-        return response.data;
+        )
+        this.token = response.data?.token
+        return response.data
       } catch (error) {
-        this.errors.login = error?.response?.data || error;
-        return false;
+        this.errors.login = error?.response?.data || error
+        return false
       } finally {
-        this.loadings.login = false;
+        this.loadings.login = false
       }
     },
     async logout() {
-      this.token = null;
-      this.user = null;
+      this.token = null
+      this.user = null
     },
     async fetchUser() {
       try {
-        this.loadings.fetchUser = true;
-        this.errors.fetchUser = null;
-        this.user = jwtDecode(this.token);
+        this.loadings.fetchUser = true
+        this.errors.fetchUser = null
+        this.user = jwtDecode(this.token)
       } catch (error) {
-        this.errors.fetchUser = error?.response?.data || error;
-        this.user = null;
-        navigateTo("/auth/login");
+        this.errors.fetchUser = error?.response?.data || error
+        this.user = null
+        navigateTo('/auth/login')
       } finally {
-        this.loadings.fetchUser = false;
+        this.loadings.fetchUser = false
       }
-    },
-  },
-});
+    }
+  }
+})
